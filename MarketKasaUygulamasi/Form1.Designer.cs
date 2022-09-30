cbedf8bf675d02098d86d0dfdb05c115959b815f
ali can yücel <yucelalican@hotmail.com>
2022-10-01 01:27:13 +0300

    Proje Tamamlandı.

--- MarketKasaUygulamasi/Form1.Designer.cs
+++ MarketKasaUygulamasi/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
             this.btnLenovoZ580 = new System.Windows.Forms.Button();
             this.btnIphoneX = new System.Windows.Forms.Button();
@@ -38,8 +39,15 @@
             this.btnKrediKarti = new System.Windows.Forms.Button();
             this.btnNakit = new System.Windows.Forms.Button();
             this.dtg1 = new System.Windows.Forms.DataGridView();
+            this.Sil = new System.Windows.Forms.DataGridViewButtonColumn();
             this.button1 = new System.Windows.Forms.Button();
+            this.gbx1 = new System.Windows.Forms.GroupBox();
+            this.btnVazgec = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.txtAdet = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dtg1)).BeginInit();
+            this.gbx1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -121,37 +129,51 @@
             // 
             // btnKrediKarti
             // 
-            this.btnKrediKarti.Location = new System.Drawing.Point(192, 393);
+            this.btnKrediKarti.Location = new System.Drawing.Point(192, 421);
             this.btnKrediKarti.Name = "btnKrediKarti";
             this.btnKrediKarti.Size = new System.Drawing.Size(90, 49);
             this.btnKrediKarti.TabIndex = 7;
             this.btnKrediKarti.TabStop = false;
             this.btnKrediKarti.Text = "Kredi Kartı";
             this.btnKrediKarti.UseVisualStyleBackColor = true;
+            this.btnKrediKarti.Click += new System.EventHandler(this.btnKrediKarti_Click);
             // 
             // btnNakit
             // 
-            this.btnNakit.Location = new System.Drawing.Point(41, 393);
+            this.btnNakit.Location = new System.Drawing.Point(29, 421);
             this.btnNakit.Name = "btnNakit";
             this.btnNakit.Size = new System.Drawing.Size(90, 49);
             this.btnNakit.TabIndex = 8;
             this.btnNakit.TabStop = false;
             this.btnNakit.Text = "Nakit";
             this.btnNakit.UseVisualStyleBackColor = true;
+            this.btnNakit.Click += new System.EventHandler(this.btnNakit_Click);
             // 
             // dtg1
             // 
             this.dtg1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtg1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Sil});
             this.dtg1.Location = new System.Drawing.Point(486, 30);
             this.dtg1.Name = "dtg1";
             this.dtg1.RowTemplate.Height = 25;
             this.dtg1.Size = new System.Drawing.Size(299, 341);
             this.dtg1.TabIndex = 9;
             this.dtg1.TabStop = false;
+            this.dtg1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtg1_CellContentClick);
+            // 
+            // Sil
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.NullValue = "Sil";
+            this.Sil.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Sil.HeaderText = "Sil";
+            this.Sil.Name = "Sil";
+            this.Sil.Text = "Sil";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(341, 393);
+            this.button1.Location = new System.Drawing.Point(341, 421);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(90, 49);
             this.button1.TabIndex = 10;
@@ -160,11 +182,63 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // gbx1
+            // 
+            this.gbx1.Controls.Add(this.btnVazgec);
+            this.gbx1.Controls.Add(this.btnKaydet);
+            this.gbx1.Controls.Add(this.txtAdet);
+            this.gbx1.Controls.Add(this.label2);
+            this.gbx1.Location = new System.Drawing.Point(173, 269);
+            this.gbx1.Name = "gbx1";
+            this.gbx1.Size = new System.Drawing.Size(218, 146);
+            this.gbx1.TabIndex = 11;
+            this.gbx1.TabStop = false;
+            this.gbx1.Text = "İşlem Yap";
+            this.gbx1.Enter += new System.EventHandler(this.gbx1_Enter);
+            // 
+            // btnVazgec
+            // 
+            this.btnVazgec.Location = new System.Drawing.Point(68, 117);
+            this.btnVazgec.Name = "btnVazgec";
+            this.btnVazgec.Size = new System.Drawing.Size(75, 23);
+            this.btnVazgec.TabIndex = 3;
+            this.btnVazgec.Text = "Vazgeç";
+            this.btnVazgec.UseVisualStyleBackColor = true;
+            this.btnVazgec.Click += new System.EventHandler(this.btnVazgec_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(68, 77);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.btnKaydet.TabIndex = 2;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // txtAdet
+            // 
+            this.txtAdet.Location = new System.Drawing.Point(45, 37);
+            this.txtAdet.Name = "txtAdet";
+            this.txtAdet.Size = new System.Drawing.Size(123, 23);
+            this.txtAdet.TabIndex = 1;
+            this.txtAdet.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Adet";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(797, 482);
+            this.Controls.Add(this.gbx1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dtg1);
             this.Controls.Add(this.btnNakit);
@@ -180,6 +254,8 @@
             this.Text = "Market Kasa Uygulaması";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dtg1)).EndInit();
+            this.gbx1.ResumeLayout(false);
+            this.gbx1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -197,5 +273,11 @@
         private Button btnNakit;
         private DataGridView dtg1;
         private Button button1;
+        private GroupBox gbx1;
+        private Button btnVazgec;
+        private Button btnKaydet;
+        private TextBox txtAdet;
+        private Label label2;
+        private DataGridViewButtonColumn Sil;
     }
 }
--- MarketKasaUygulamasi/Form1.cs
+++ MarketKasaUygulamasi/Form1.cs
@@ -1,9 +1,11 @@
+﻿using Excel = Microsoft.Office.Interop.Excel;
 namespace MarketKasaUygulamasi
 {
     public partial class Form1 : Form
     {
         decimal total = 0;
         List<Sepet> Sepetim = new List<Sepet>();
+        Sepet sepet = new Sepet();
         public Form1()
         {
             InitializeComponent();
@@ -21,14 +23,57 @@ namespace MarketKasaUygulamasi
 
            
         }
+        void MesajGoster(string message)
+        {
+            if (Sepetim.Count > 0)
+            {
+                FisOlustur(message);
+                return;
+                Sepetim = new List<Sepet>();
+                SepetiGoster();
+                MessageBox.Show(message, "Ödeme Yapýldý", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Sepetiniz boþ", "Hata!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
 
         private void btnLenovoZ580_Click(object sender, EventArgs e)
         {
-            Sepet sepet = new Sepet(1,"Lenovo Z580",1450);
-            Sepetim.Add(sepet);
-
+            butonlariKapat();
+            sepet = new Sepet(1,"Lenovo Z580",1450);
             SepetiGoster();
             SepetToplam();
+        }
+        void Vazgec()
+        {
+            txtAdet.Text = "1";
+            gbx1.Visible = false;
+            bool status = true;
+            btnIphoneX.Enabled = status;
+            btnLenovoZ580.Enabled = status;
+            btnNakit.Enabled = status;
+            btnOppoReno.Enabled = status;
+            btnKrediKarti.Enabled = status;
+            btnMackbookPro.Enabled = status;
+            btnLgG2.Enabled = status;
+            btnSamsungF20.Enabled = status;
+        }
+        void butonlariKapat()
+        {
+            gbx1.Visible = true;
+            bool status=false;
+            btnIphoneX.Enabled = status;
+            btnLenovoZ580.Enabled = status;
+            btnNakit.Enabled = status;
+            btnOppoReno.Enabled = status;
+            btnKrediKarti.Enabled = status;
+            btnMackbookPro.Enabled = status;
+            btnLgG2.Enabled = status;
+            btnSamsungF20.Enabled = status;
+
         }
         void SepetiGoster()
         {
@@ -51,8 +96,9 @@ namespace MarketKasaUygulamasi
 
         private void btnIphoneX_Click(object sender, EventArgs e)
         {
-            Sepet sepet = new Sepet(1, "Iphone X", 22250);
-            Sepetim.Add(sepet);
+            butonlariKapat();
+             sepet = new Sepet(1, "Iphone X", 22250);
+        
 
             SepetiGoster();
             SepetToplam();
@@ -60,8 +106,9 @@ namespace MarketKasaUygulamasi
 
         private void btnLgG2_Click(object sender, EventArgs e)
         {
-            Sepet sepet = new Sepet(1, "LG G2", 1280);
-            Sepetim.Add(sepet);
+            butonlariKapat();
+             sepet = new Sepet(1, "LG G2", 1280);
+           
 
             SepetiGoster();
             SepetToplam();
@@ -69,26 +116,28 @@ namespace MarketKasaUygulamasi
 
         private void btnSamsungF20_Click(object sender, EventArgs e)
         {
-            Sepet sepet = new Sepet(1, "Samsung F20", 10450);
-            Sepetim.Add(sepet);
-
+            butonlariKapat();
+            sepet = new Sepet(1, "Samsung F20", 10450);
+            
             SepetiGoster();
             SepetToplam();
         }
 
         private void btnMackbookPro_Click(object sender, EventArgs e)
         {
-            Sepet sepet = new Sepet(1, "MackbookPro", 20500);
-            Sepetim.Add(sepet);
-
+            butonlariKapat();
+             sepet = new Sepet(1, "MackbookPro", 20500);
+           
+           
             SepetiGoster();
             SepetToplam();
         }
 
         private void btnOppoReno_Click(object sender, EventArgs e)
         {
-            Sepet sepet = new Sepet(1, "Oppo Reno", 7500);
-            Sepetim.Add(sepet);
+            butonlariKapat();
+           sepet = new Sepet(1, "Oppo Reno", 7500);
+          
 
             SepetiGoster();
             SepetToplam();
@@ -97,6 +146,106 @@ namespace MarketKasaUygulamasi
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btnVazgec_Click(object sender, EventArgs e)
+        {
+            Vazgec();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            sepet.Adet = Convert.ToInt32(txtAdet.Text);
+            sepet.Toplam =(decimal) sepet.Adet * sepet.BirimFiyat;
+            Sepetim.Add(sepet);
+            SepetiGoster();
+            Vazgec();
+        }
+
+        private void dtg1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var senderGrid = (DataGridView)sender;
+            if (senderGrid.Columns[e.ColumnIndex]is DataGridViewButtonColumn && e.RowIndex>=0)
+            {
+               string urunadi = dtg1.Rows[e.RowIndex].Cells["UrunAdi"].Value.ToString();
+                if (MessageBox.Show($"{urunadi}urunu silmek istiyormusunuz","Sil?",MessageBoxButtons.YesNo,MessageBoxIcon.Question)==DialogResult.Yes)
+                {
+                    Sepetim.RemoveAt(e.RowIndex);
+                    SepetiGoster();
+                }
+                
+           
+            }
+        }
+        void FisOlustur(string message)
+        {
+            Excel.Application excel = new Excel.Application();
+            object Missing = Type.Missing;
+            Excel.Workbook workbook = excel.Workbooks.Add(Missing);
+            Excel.Worksheet worksheet = (Excel.Worksheet)excel.Sheets[1];
+
+            worksheet.Range["A1"].Value = "#";
+            worksheet.Range["B1"].Value = "Ürün Adi";
+            worksheet.Range["C1"].Value = "Adet";
+            worksheet.Range["D1"].Value = "Birim Fiyati";
+            worksheet.Range["E1"].Value = "Toplam Tutar";
+
+            worksheet.Range["A1"].ColumnWidth = 3.43;
+            worksheet.Range["B1"].ColumnWidth = 13.29;
+            worksheet.Range["C1"].ColumnWidth = 4.57;
+            worksheet.Range["D1"].ColumnWidth = 10.57;
+            worksheet.Range["E1"].ColumnWidth = 13.71;
+
+            worksheet.Range["A1:E1"].Font.ColorIndex = 3;
+
+            int rowCount = worksheet.Range["A" + worksheet.Rows.Count].End[Excel.XlDirection.xlUp].Row;
+
+            rowCount++;
+            for (int x = 0; x < Sepetim.Count; x++)
+            {
+                worksheet.Range["A" + rowCount].Value = (x + 1);
+                worksheet.Range["B" + rowCount].Value = Sepetim[x].UrunAdi;
+                worksheet.Range["C" + rowCount].Value = Sepetim[x].Adet;
+                worksheet.Range["D" + rowCount].Value = Sepetim[x].BirimFiyat;
+                worksheet.Range["E" + rowCount].Value = Sepetim[x].Toplam;
+                rowCount++;
+            }
+
+            worksheet.Range["A1:E" + (rowCount - 1)].Borders.LineStyle = 1;
+
+            worksheet.Range["E" + rowCount].Formula = "=Sum(E2:E" + (rowCount - 1) + ")";
+            worksheet.Range["E" + rowCount].Borders.LineStyle = 1;
+            rowCount++;
+
+            worksheet.Range["A" + rowCount].Value = message;
+            worksheet.Range["A" + rowCount + ":E" + rowCount].MergeCells = true;
+            worksheet.Range["A" + rowCount].HorizontalAlignment = 3;
+            worksheet.Range["A" + rowCount].VerticalAlignment = 2;
+            worksheet.Range["A" + rowCount].Font.ColorIndex = 3;
+            worksheet.Range["A" + rowCount].Font.Bold = true;
+
+            worksheet.Range["D2:E" + rowCount].NumberFormat = "#,##0.00";
+
+            // worksheet.PrintOutEx();
+
+            workbook.SaveAs(@"C:\1\Test.xlsx");
+
+            excel.Visible = true;
+        }
+
+        private void gbx1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNakit_Click(object sender, EventArgs e)
+        {
+            MesajGoster("Sepetteki ürünler nakit olarak ödendi");
+        }
+
+        private void btnKrediKarti_Click(object sender, EventArgs e)
+        {
+            MesajGoster("Sepetteki ürünler kredi kartý ile ödendi");
         }
     }
 }
